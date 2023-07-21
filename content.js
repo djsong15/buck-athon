62e689c01828f065b66bd1a182cdd2ad728fea5e
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-20 20:09:27 -0700

    Started on content file to manipulate DOM of webpage

---
+++ content.js
@@ -0,0 +1,19 @@
+function imageElementGrab(){
+    // console.log('test inside of imageElement')
+    // const expression = "//img"
+    // const xpathResult = document.evaluate(expression, document, null, XPathResult.ANY_TYPE, null);
+    // const arr = [];
+    // let node = xpathResult.iterateNext();
+    // while(node){
+    //     arr.push(node);
+    //     node = xpathResult.iterateNext();
+    // }
+    // console.log(arr);
+    const img = document.querySelectorAll('img');
+    // const test = document.querySelector('#conversion-ui')
+    for(let i =0; i<img.length; i++){
+        img[i].src = 'images/japanflag.png'
+    }
+
+}
+imageElementGrab();
