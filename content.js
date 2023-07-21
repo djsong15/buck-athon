44de7d8b71f6999f578fe75334a3a932e557e8de
Albert Hu <albhu24@gmail.com>
2023-07-21 16:07:22 -0700

    first initial commit after fork and clone from dsong

--- content.js
+++ content.js
@@ -1,24 +1,21 @@
-function imageElementGrab(){
-    // console.log('test inside of imageElement')
-    // const expression = "//img"
-    // const xpathResult = document.evaluate(expression, document, null, XPathResult.ANY_TYPE, null);
-    // const arr = [];
-    // let node = xpathResult.iterateNext();
-    // while(node){
-    //     arr.push(node);
-    //     node = xpathResult.iterateNext();
-    // }
-    // console.log(arr);
-    const img = document.querySelectorAll('img');
-    // const test = document.querySelector('#conversion-ui')
-    for (let i = 0; i < img.length; i++){
-        img[i].src = 'images/japanflag.png'
-    }
-
+function imageElementGrab() {
+  // console.log('test inside of imageElement')
+  // const expression = "//img"
+  // const xpathResult = document.evaluate(expression, document, null, XPathResult.ANY_TYPE, null);
+  // const arr = [];
+  // let node = xpathResult.iterateNext();
+  // while(node){
+  //     arr.push(node);
+  //     node = xpathResult.iterateNext();
+  // }
+  // console.log(arr);
+  const img = document.querySelectorAll("img");
+  // const test = document.querySelector('#conversion-ui')
+  for (let i = 0; i < img.length; i++) {
+    img[i].src = "images/japanflag.png";
+  }
 }
 // imageElementGrab();
-chrome.runtime.onMessage.addListener(
-    function (request) {
-        if (request.message === 'start') imageElementGrab();
-    }
-)
+chrome.runtime.onMessage.addListener(function (request) {
+  if (request.message === "start") imageElementGrab();
+});
