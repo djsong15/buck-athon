1ac58c0eba64e96e08b5b33d7daf72795dd1765d
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-22 08:40:55 -0700

    Updating popup-script with new script

--- popup/popup-script.js
+++ popup/popup-script.js
@@ -1,3 +1,58 @@
+// chrome.runtime.onMessage.addListener(function (message) {
+//   const contentData = message.data;
+//   document
+//     .querySelector("#calculate")
+//     .addEventListener("click", calculate(contentData));
+// });
+
+const currencySymbols = {
+  'USD': '$',
+  'EUR': '€',
+  'JPY': '¥',
+  'GBP': '£',
+  'CNY': '¥',
+  'AUD': '$',
+  'CAD': '$',
+  'CHF': 'CHF',
+  'HKD': '$',
+  'SGD': '$',
+  'SEK': 'kr',
+  'KRW': '₩',
+  'NOK': 'kr',
+  'NZD': '$',
+  'INR': '₹'
+}
+
+async function calculate() {
+  const currAmt = document.querySelector('#currency-amt').value;
+  if (!currAmt) return;
+  const fromCurr = document.querySelector("#from-currency").value;
+  const toCurr = document.querySelector("#to-currency").value;
+  const result = await fetch(
+    `https://api.exchangerate.host/convert?from=${fromCurr}&to=${toCurr}&amount=${currAmt}`
+  ).then((data) => data.json());
+  let toCurrSym = currencySymbols[toCurr];
+  let roundedResult = result.result.toFixed(2);
+  let convertedAmt = toCurrSym.length === 1 ? `${toCurrSym}${roundedResult}` : `${roundedResult} ${toCurrSym}`;
+  console.log(convertedAmt);
+
+  document.querySelector("#result").textContent = convertedAmt;
+  // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  //   let activeTab = tabs[0];
+  //   chrome.tabs.sendMessage(activeTab.id, { message: "start" });
+  // });
+  return convertedAmt;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('#convert').addEventListener('click', () => {
+    chrome.tabs.query({currentWindow: true, active: true}, (tabs) => {
+      let activeTab = tabs[0];
+      chrome.tabs.sendMessage(activeTab.id, {"toConv": document.querySelector("#to-currency").value});
+      console.log('sent');
+    });
+  });
+})
 
 document.querySelector('#calculate').addEventListener('click', calculate);
 document.querySelector('#to-currency').addEventListener('change', calculate);
