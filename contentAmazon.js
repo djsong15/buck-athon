2f20da6a29dff9fa8671316bfc1f26f049cd6e51
Albert Hu <albhu24@gmail.com>
2023-07-21 20:41:38 -0700

    main functionality works but need progress on minimization and convert button

--- contentAmazon.js
+++ contentAmazon.js
@@ -7,10 +7,13 @@ async function getSelectionText() {
   } else if (document.selection && document.selection.type != "Control") {
     text = document.selection.createRange().text;
   }
+  console.log(text);
+  console.log(typeof text);
   const amtToConvert = Number(text);
   console.log(amtToConvert);
+  console.log(typeof amtToConvert);
+
   const response = await chrome.runtime.sendMessage({
     data: amtToConvert,
   });
-  console.log(response);
 }
--- popup/popup-script.js
+++ popup/popup-script.js
@@ -1,24 +1,54 @@
 chrome.runtime.onMessage.addListener(function (message) {
   const contentData = message.data;
-  document
-    .querySelector("#calculate")
-    .addEventListener("click", calculate(contentData));
+  console.log(contentData);
+  console.log(typeof contentData);
+  //   calculate = document.querySelector("#calculate");
+  calculate(contentData);
 });
 
-async function calculate(data) {
-  const currAmt = data;
-  console.log(data);
-  document.querySelector("#currency-amt").value = data;
+async function calculate(contentData) {
+  console.log(contentData, "contentData");
+  const currAmt = contentData;
+  document.querySelector("#currency-amt").value = contentData;
   const fromCurr = document.querySelector("#from-currency").value;
   const toCurr = document.querySelector("#to-currency").value;
   const result = await fetch(
     `https://api.exchangerate.host/convert?from=${fromCurr}&to=${toCurr}&amount=${currAmt}`
   ).then((data) => data.json());
+  console.log(result, "result");
   console.log(result.result);
-
   document.querySelector("#result").textContent = result.result;
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    let activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: "start" });
-  });
 }
+
+// calculate.addEventListener("click", async (contentData) => {
+//     console.log(contentData, "contentData");
+//     const currAmt = contentData;
+//     document.querySelector("#currency-amt").value = contentData;
+//     const fromCurr = document.querySelector("#from-currency").value;
+//     const toCurr = document.querySelector("#to-currency").value;
+//     const result = await fetch(
+//       `https://api.exchangerate.host/convert?from=${fromCurr}&to=${toCurr}&amount=${currAmt}`
+//     ).then((data) => data.json());
+//     console.log(result, "result");
+//     console.log(result.result);
+//     document.querySelector("#result").textContent = result.result;
+//   });
+
+// async function calculate(data) {
+//   console.log(data, "this should be an object pointerevent");
+//   const currAmt = data;
+//   console.log(data);
+//   document.querySelector("#currency-amt").value = data;
+//   const fromCurr = document.querySelector("#from-currency").value;
+//   const toCurr = document.querySelector("#to-currency").value;
+//   const result = await fetch(
+//     `https://api.exchangerate.host/convert?from=${fromCurr}&to=${toCurr}&amount=${currAmt}`
+//   ).then((data) => data.json());
+//   console.log(result.result);
+//   document.querySelector("#result").textContent = result.result;
+
+//   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+//     let activeTab = tabs[0];
+//     chrome.tabs.sendMessage(activeTab.id, { message: "start" });
+//   });
+// }
