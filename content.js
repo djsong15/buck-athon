0564ee796e2b1ef2854d8e95d30fdff9dd7110c2
Daniel Song <81377675+djsong15@users.noreply.github.com>
2023-07-22 08:30:07 -0700

    Script now pulls price tag from webpage to be used to convert into a new currency. content.js now receives messages from popup-script to determine which currency to conver to

--- content.js
+++ content.js
@@ -26,3 +26,47 @@
 //   });
 //   console.log(response);
 // }
+const price = document.querySelector('span[itemprop=price]').textContent; //document.querySelector('main>div:nth-child(2)>div:nth-child(2)>div>div>div>div>div:nth-child(2)>div>div>div:nth-child(2)>div>div>div>span>span:nth-child(2)>span').textContent;
+console.log(price.slice(price.indexOf('$')));
+let index = price.indexOf('$') + 1;
+
+chrome.runtime.onMessage.addListener(
+  async function (request) {
+    console.log(`received, convert ${price.slice(index)} to ${request.toConv}`);
+    const newPrice = await updatePrice(price.slice(index), request.toConv);
+    // document.querySelector('main>div:nth-child(2)>div:nth-child(2)>div>div>div>div>div:nth-child(2)>div>div>div:nth-child(2)>div>div>div>span>span:nth-child(2)>span').textContent = newPrice;
+    document.querySelector('span[itemprop=price]').textContent = newPrice;
+  }
+);
+
+const currencySymbols = {
+  'USD': '$',
+  'EUR': '€',
+  'JPY': '¥',
+  'GBP': '£',
+  'CNY': '¥',
+  'AUD': '$',
+  'CAD': '$',
+  'CHF': 'CHF',
+  'HKD': '$',
+  'SGD': '$',
+  'SEK': 'kr',
+  'KRW': '₩',
+  'NOK': 'kr',
+  'NZD': '$',
+  'INR': '₹'
+}
+
+async function updatePrice(price, toCurr) {
+  console.log(price, toCurr);
+  const result = await fetch(
+    `https://api.exchangerate.host/convert?from=USD&to=${toCurr}&amount=${price}`
+  ).then((data) => data.json());
+  let toCurrSym = currencySymbols[toCurr];
+  console.log(result.result);
+  let roundedResult = result.result.toFixed(2);
+  let convertedAmt = toCurrSym.length === 1 ? `${toCurrSym}${roundedResult}` : `${roundedResult} ${toCurrSym}`;
+  console.log(convertedAmt);
+
+  return convertedAmt;
+}
